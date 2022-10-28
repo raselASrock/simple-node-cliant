04a7b4e02c2c3395232a2bbbed1a009c72f28f7d
raSel <mdraselm413@gmail.com>
2022-10-28 23:31:57 +0600

    form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+const [users, setUsers] = useState([]);
+
+useEffect(() =>{
+  fetch("http://localhost:5000/users")
+  .then(res => res.json())
+  .then(data => setUsers(data))
+},[])
+
+const handleAddUser = event =>{
+  event.preventDefault()
+  
+}
+
   return (
     <div className="App">
-      
+      <form onSubmit={handleAddUser}>
+        <input type="text" name='name' placeholder='Your Name' />
+        <br />
+        <input type="email" name="email" id="" placeholder='Your Email Id' />
+        <br />
+        <button type='submit'>Add User</button>
+      </form>
+      <h2>Users: {users.length}</h2>
+      <div>
+        {
+          users.map(user => <p key={user.id}>{user.name} {user.email}</p>)
+        }
+      </div>
     </div>
   );
 }
