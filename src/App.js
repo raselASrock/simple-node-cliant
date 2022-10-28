e6638791f14ab2a991bea92d75b3e7712af51b45
raSel <mdraselm413@gmail.com>
2022-10-28 23:55:46 +0600

    api called

--- src/App.js
+++ src/App.js
@@ -14,7 +14,22 @@ useEffect(() =>{
 
 const handleAddUser = event =>{
   event.preventDefault()
-  
+  const form = event.target;
+  const name = form.name.value;
+  const email = form.email.value;
+  const user = {name, email};
+  console.log(user);
+  fetch("http://localhost:5000/users", {
+    method:"POST",
+    headers: {
+      'content-type': 'application/json' 
+    },
+    body: JSON.stringify(user)
+  })
+  .then(res => res.json())
+  .then(data => console.log(data))
+  .catch(error => console.error(error))
+  form.reset()
 }
 
   return (
